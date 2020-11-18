2c432641731599fb2b9a0f3f8a1b3635ec03f10e
aleksa <onshiftmusic@gmail.com>
2020-11-18 19:35:51 +0100

    [npm] add html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     entry: './app/app.js',
@@ -6,6 +7,15 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: 'html-loader'
+            }
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({template: './dist/index.html'})],
     devServer: {
         port: 8000,
         contentBase: path.resolve(__dirname, 'dist'),
