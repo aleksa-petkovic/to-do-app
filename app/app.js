558d35584c5f216322946836201b7f35c4724442
aleksa <onshiftmusic@gmail.com>
2020-11-26 12:20:41 +0100

    Add tasks page module in app.js

--- app/app.js
+++ app/app.js
@@ -1,11 +1,6 @@
-import angular from 'angular'
-
-import textInputModule from './components/text-input/text-input.module';
-import checkboxModule from './components/checkbox/checkbox.module';
-import buttonModule from './components/button/button.module';
+import angular from 'angular';
+import tasksPageModule from './components/pages/tasks-page/tasks-page.module';
 
 export default angular.module('TodoApp', [
-    textInputModule,
-    checkboxModule,
-    buttonModule
+    tasksPageModule
   ]);
