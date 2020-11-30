7bbecadaab6f26b245ee9c9be0b0b87fc9eb6e53
aleksa <onshiftmusic@gmail.com>
2020-11-30 15:27:19 +0100

    Add routes

--- app/app.js
+++ app/app.js
@@ -1,6 +1,22 @@
 import angular from 'angular';
+import ngRoute from 'angular-route';
+import editTaskPageModule from './components/pages/edit-task/edit-task.module';
 import tasksPageModule from './components/pages/tasks-page/tasks-page.module';
 
 export default angular.module('TodoApp', [
-    tasksPageModule
+    tasksPageModule,
+    editTaskPageModule,
+    ngRoute
+  ]).config(['$routeProvider',
+    function config($routeProvider) {
+      'ngInject'
+      $routeProvider.
+        when('/', {
+          template: '<tasks-page></tasks-page>'
+        }).
+        when('/edit/:taskId', {
+          template: '<edit-task></edit-task>'
+        }).
+        otherwise('/');
+    }
   ]);
